b999d9c23c7bf915e7ca802139316027092ddc4b
Andrebond777 <41829674+Andrebond777@users.noreply.github.com>
2021-02-17 14:38:59 +0300

    17.02

---
+++ C5 - Delegate/Delegate (17)/Delegate/CountDown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class CountDown
+    {
+        private string _message;
+        private int _milliseconds;
+        public CountDown(string message, int seconds)
+        {
+            _message = message;
+            _milliseconds = seconds * 1000;
+        }
+        public void Count(Action<string> Observer)
+        {
+            Task.Delay(_milliseconds).Wait();
+            Observer(_message);
+        }
+    }
+}
---
+++ C5 - Delegate/Delegate (17)/Delegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<String> Observer = message => Console.WriteLine(message);
+            var counter = new CountDown("Hello", 0);
+            counter.Count(Observer);
+        }
+    }
+}
